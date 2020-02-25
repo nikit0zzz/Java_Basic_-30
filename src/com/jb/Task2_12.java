b06dd1c16de29f8ed2ad69086fdcc90ae613c12f
Nikita_Zubkov <Nikita_Zubkov@epam.com>
2020-02-25 18:40:08 +0300

    Unit02

--- src/com/jb/Task2_12.java
+++ src/com/jb/Task2_12.java
@@ -13,14 +13,14 @@ public class Task2_12 {
 //        String s  = scanner.nextLine();
         String s = "strIng01";
         System.out.println(s);
-        if (Pattern.matches("[^_a-zA-Z]{1}", s)) {
-            if (Pattern.matches("[a-zA-Z0-9]+", s)) {
+        if (Pattern.matches("^[_\\D&&\\w][\\w]*$", s)) {
+            if (Pattern.matches(".[a-zA-Z0-9]*$", s)) {
                 System.out.println("ok");
             } else {
                 System.out.println("Набор символов не соответствует, должно быть нижнее подчеркивание или буквы или цифры");
             }
         } else {
-            System.out.println("Первый символ не соотвествует, должен быть или нижнее подчеркивание или буква");
+            System.out.println("Первый символ не соотвествует,первым символом должно быть или нижнее подчеркивание или буква");
         }
     }
 }
---
+++ src/com/jb/Task2_15.java
@@ -0,0 +1,12 @@
+//Найдите количество четных цифр данного натурального числа
+package com.jb;
+
+import java.util.Scanner;
+
+public class Task2_15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число:");
+        String s = scanner.nextLine();
+    }
+}
