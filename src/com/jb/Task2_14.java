0d9f8ef7a0576f00b65d9c36a15daa3eb4e0f13f
Nikita_Zubkov <Nikita_Zubkov@epam.com>
2020-02-28 13:04:04 +0300

    Unit02

--- src/com/jb/Task2_14.java
+++ src/com/jb/Task2_14.java
@@ -4,10 +4,10 @@ package com.jb;
 public class Task2_14 {
     public static void main(String[] args) {
         int i, j, k ,l , sum;
-        for (i = 1; i <= 9; i++){
-            for (j = 1; j <= 9; j++){
-                for (k = 1; k <= 9; k++){
-                    for (l = 1; l <= 9; l++){
+        for (i = 0; i <= 9; i++){
+            for (j = 0; j <= 9; j++){
+                for (k = 0; k <= 9; k++){
+                    for (l = 0; l <= 9; l++){
                         sum = i + j + k + l;
                         if (sum == 15){
                             System.out.println("Число, цифр каждого из которых равна 15 это " + i + j + k + l + "; " + i + "+" + j + "+" + k + "+" + l + " = 15");
