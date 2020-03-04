75542f2b91a97895966a4c22ff9b0e0733f736db
nikit0zzz <ryazan3@gmail.com>
2020-03-04 21:55:10 +0300

    Unit03

--- src/com/jb/MultiplicationTable.java
+++ src/com/jb/MultiplicationTable.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        long examples = Math.round(Math.random()*(25-5) + 1);
+        long examples = Math.round(Math.random()*(25-15) + 1);
         int right_answers = 0;
         int bad_answer = 0;
         for (int i = 0; i < examples; i++){
@@ -20,17 +20,17 @@ public class MultiplicationTable {
                 System.out.println("Верно!");
                 right_answers++;
             } else {
-                System.out.println("Не правильный ответ!");
+                System.out.println("Неудачник!");
                 bad_answer++;
             }
         }
-        System.out.println("Вы ответили правильно " + right_answers + " раз");
-        System.out.println("Вы ответили не правильно " + bad_answer + " раз");
-        System.out.println("Вы правильно ответили на " + (Math.round((Math.floor(right_answers)/Math.floor(examples) * 100))) +"% вопросов");
+        System.out.println("Вы дали правильный ответ " + right_answers + " раз");
+        System.out.println("Вы дали правильный ответ " + bad_answer + " раз");
+        System.out.println("Вы дали правильный ответ в " + (Math.round((Math.floor(right_answers)/Math.floor(examples) * 100))) +"% случаев");
         if(Math.round((Math.floor(right_answers)/Math.floor(examples) * 100)) <= 97){
-            System.out.println("Вы правильно ответили на " + (Math.round((Math.floor(right_answers)/Math.floor(examples) * 100))) +"% вопросов и вы чмо необразованное");
+            System.out.println("Вы дали правильный ответ в " + (Math.round((Math.floor(right_answers)/Math.floor(examples) * 100))) +"% случаев и вы чмо необразованное");
         } else {
-            System.out.println("Вы правильно ответили на " + (Math.round((Math.floor(right_answers)/Math.floor(examples) * 100))) + "% вопросов и вы Эйнштейн!!!");
+            System.out.println("Вы дали правильный ответ в " + (Math.round((Math.floor(right_answers)/Math.floor(examples) * 100))) + "% случаев и вы Эйнштейн!!!");
         }
     }
 }
