115e0d53505fe4876612f7ee27a10e12afb7da5a
nikit0zzz <ryazan3@gmail.com>
2020-03-04 21:52:00 +0300

    Unit03

--- src/com/jb/MultiplicationTable.java
+++ src/com/jb/MultiplicationTable.java
@@ -1,35 +1,36 @@
+//Напишите консольное приложение, проверяющее знание таблицы умножения.
+
 package com.jb;
 
 import java.util.Scanner;
 import java.util.Random;
-import java.util.regex.Matcher;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        long examples = Math.round(Math.random()*(10-3));
+        long examples = Math.round(Math.random()*(25-5) + 1);
         int right_answers = 0;
         int bad_answer = 0;
         for (int i = 0; i < examples; i++){
             long a = Math.round(Math.random()*(9-1) + 1);
             long b = Math.round(Math.random()*(9-1) + 1);
-            System.out.println(a + " * " + b + "= ?");
+            System.out.println(a + " x " + b + " = ?");
             int answer = scanner.nextInt();
             if (a * b == answer){
                 System.out.println("Верно!");
                 right_answers++;
             } else {
-                System.out.println("Неудачник!");
+                System.out.println("Не правильный ответ!");
                 bad_answer++;
             }
         }
-        System.out.println("right_answers = " + right_answers);
-        System.out.println("bad_answer = " + bad_answer);
+        System.out.println("Вы ответили правильно " + right_answers + " раз");
+        System.out.println("Вы ответили не правильно " + bad_answer + " раз");
         System.out.println("Вы правильно ответили на " + (Math.round((Math.floor(right_answers)/Math.floor(examples) * 100))) +"% вопросов");
         if(Math.round((Math.floor(right_answers)/Math.floor(examples) * 100)) <= 97){
-            System.out.println("Чмо необразованное");
+            System.out.println("Вы правильно ответили на " + (Math.round((Math.floor(right_answers)/Math.floor(examples) * 100))) +"% вопросов и вы чмо необразованное");
         } else {
-            System.out.println("Эйнштейн!!!");
+            System.out.println("Вы правильно ответили на " + (Math.round((Math.floor(right_answers)/Math.floor(examples) * 100))) + "% вопросов и вы Эйнштейн!!!");
         }
     }
 }
